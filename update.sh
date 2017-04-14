a49c6f0557b5e2ce4d1e2b3a08c386ab5d60c359
Wes Morgan <wesmorgan@icloud.com>
2017-04-14 15:02:16 -0600

    Add fabric8 variants

--- update.sh
+++ update.sh
@@ -14,12 +14,24 @@ OPENJDK_VERSION=8
 PAUL='Paul Lam <paul@quantisan.com>'
 WES='Wes Morgan <wesmorgan@icloud.com>'
 
+declare -A base_images=(
+  [alpine]=openjdk
+  [debian]=openjdk
+  [fabric8]=fabric8/java-centos-openjdk8-jdk
+)
+
+declare -A base_tags=(
+  [alpine]=$OPENJDK_VERSION
+  [debian]=$OPENJDK_VERSION
+  [fabric8]=latest
+)
+
 declare -A maintainers=(
   [debian/lein]=$PAUL
   [debian/boot]=$WES
   [alpine]=$WES
+  [fabric8]=$WES
 )
-
 
 # Dockerfile generator
 
@@ -41,27 +53,31 @@ EOH
 }
 
 for variant in "${variants[@]}"; do
-  openjdk_version=$OPENJDK_VERSION
   dir="$variant"
   base_variant=${variant%/*}
+  base_image=${base_images[$base_variant]}
+  base_tag=${base_tags[$base_variant]}
   build_tool=${variant#*/}
   echo "Generating Dockerfile for $dir"
   [ -d "$dir" ] || continue
   template="Dockerfile-$build_tool.template"
   echo "Using template $template"
-  if [ "$base_variant" != "debian" ]; then
-    openjdk_version="${openjdk_version}-${base_variant}"
+  if [ "$base_variant" = "alpine" ]; then
+    base_tag="${base_tag}-${base_variant}"
   fi
   maintainer=${maintainers[$variant]:-${maintainers[$base_variant]}}
   { generated_warning; cat "$template"; } > "$dir/Dockerfile"
   ( set -x
-    sed -i '' 's!%%BASE_TAG%%!'"$openjdk_version"'!g' "$dir/Dockerfile"
+    sed -i '' 's!%%BASE_IMAGE%%!'"$base_image"'!g' "$dir/Dockerfile"
+    sed -i '' 's!%%BASE_TAG%%!'"$base_tag"'!g' "$dir/Dockerfile"
     sed -i '' 's!%%MAINTAINER%%!'"$maintainer"'!g' "$dir/Dockerfile"
     if [ "$base_variant" = "alpine" ]; then
       sed -i '' 's/^%%ALPINE%% //g' "$dir/Dockerfile"
-    else
-      sed -i '' '/^%%ALPINE%%/d' "$dir/Dockerfile"
-      sed -i '' '/^$/N;/^\n$/D' "$dir/Dockerfile"
+    elif [ "$base_variant" = "fabric8" ]; then
+      sed -i '' 's/^%%FABRIC8%% //g' "$dir/Dockerfile"
     fi
+    sed -i '' '/^%%ALPINE%%/d' "$dir/Dockerfile"
+    sed -i '' '/^%%FABRIC8%%/d' "$dir/Dockerfile"
+    sed -i '' '/^$/N;/^\n$/D' "$dir/Dockerfile"
   )
 done
